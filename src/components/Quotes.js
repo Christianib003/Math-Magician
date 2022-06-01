d3ffac877efa58d3720ab832331a73e719ce7a27
Christianib003 <christianib003@gmail.com>
2022-06-01 15:50:34 +0200

    add all three pages

---
+++ src/components/Quotes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Quotes = () => (
+  <>
+    <div className="quotes">
+      <p className="qtext">
+        1. “The purpose of our lives is to be happy.” —
+        <span> Dalai Lama</span>
+      </p>
+      <p className="qtext">
+        2. “Get busy living or get busy dying.” —
+        <span> Stephen King</span>
+      </p>
+      <p className="qtext">
+        3. “You only live once, but if you do it right, once is enough.” —
+        <span> Mae West</span>
+      </p>
+    </div>
+  </>
+);
+
+export default Quotes;
