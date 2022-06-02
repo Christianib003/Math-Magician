03b953e1e212f580e87f0601b387a3de6b286a49
myelin0 <emi.veisllari@gmail.com>
2022-06-02 15:06:41 +0200

    Add unit test for Operate and Solve files

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,16 @@ const Home = () => (
         except to obtain some advantage from it? But who has right to find fault with a man
       </p>
       <p className="text">
-      On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble ; and equal blame belongs to those who fail in their duty through weakness of will. as saying through shrinking from toil and pain. In a free hour, when our power of choice is untrammelled being able to do what we like best, every pleasure is to be welcomed of duty or the obligations of business it will frequently occur The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures
+        On the other hand, we denounce with righteous indignation
+        and dislike men who are so beguiled and demoralized by the
+        charms of pleasure of the moment, so blinded by desire,
+        that they cannot foresee the pain and trouble ; and equal
+        blame belongs to those who fail in their duty through weakness of will.
+        as saying through shrinking from toil and pain. In a free hour,
+        when our power of choice is untrammelled being able to do what we like best,
+        every pleasure is to be welcomed of duty or the obligations of business it will
+        frequently occur The wise man therefore always holds in these matters to this
+        principle of selection: he rejects pleasures to secure other greater pleasures
       </p>
     </div>
   </>
